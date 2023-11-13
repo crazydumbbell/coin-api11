e049fc68f9a7266201bf13659c6af2624f396340
crazydumbbell <wogus5357@gmail.com>
2023-11-13 09:16:32 +0900

    tt

--- src/components/CoinCard.jsx
+++ src/components/CoinCard.jsx
@@ -6,8 +6,9 @@ const CoinCard = ({ coinPrice, btcPrice }) => {
         //블록이라서 인라인블록으로 바꿈 이미지 위에 붙으니까 옆으로
         src={
           process.env.PUBLIC_URL +
-          `/images/${coinPrice.market.substring(4).toLowercase()}.png`
+          `/images/${coinPrice.market.substring(4).toLowerCase()}.png`
         }
+        //vercel 배포했을때 이미지 안뜨는것은 위에 이미지를 대소문자 구분해버려서 전부 소문자로 바꿔주는 toLowerCase 해주면됨
         alt={`${coinPrice.market.substring(4)}`}
         // 알트값은 이미지 안뜰때 뜨는 값//
       />
